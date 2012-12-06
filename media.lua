94f1586f89d2e265654cb0611291a37a80bf14fa
andy <andy@stormbase.net>
2012-12-06 23:49:38 +0000

    media: added normal and unitframes fonts

--- media.lua
+++ media.lua
@@ -14,11 +14,21 @@ local Media = {
 		this.colors = {
 			shadow =  {0, 0, 0, 0.8},
 			background = {0, 0, 0, 0.4},
+			reaction = {
+				[1] = { 222/255, 95/255,  95/255 }, -- Hated
+				[2] = { 222/255, 95/255,  95/255 }, -- Hostile
+				[3] = { 222/255, 95/255,  95/255 }, -- Unfriendly
+				[4] = { 218/255, 197/255, 92/255 }, -- Neutral
+				[5] = { 75/255,  175/255, 76/255 }, -- Friendly
+				[6] = { 75/255,  175/255, 76/255 }, -- Honored
+				[7] = { 75/255,  175/255, 76/255 }, -- Revered
+				[8] = { 75/255,  175/255, 76/255 }, -- Exalted	
+			},
 		}
 
 		this.fonts = {
-			normal = [[Interface\AddOns\Darkui\media\fonts\PT-Sans-Narrow.ttf]],
-			--normal = [[Interface\AddOns\Darkui\media\fonts\PT-Sans-Narrow.ttf]],
+			normal = [[Interface\AddOns\Dark.Core\media\fonts\PT-Sans-Narrow.ttf]],
+			unitframes = [[Interface\AddOns\Darkui\media\fonts\BigNoodleTitling.ttf]],
 		}
 
 		return this 
