f2322d67b6cd705b20eb61c1a7045b1e2fbd1a41
andy <andy@andydote.co.uk>
2014-02-15 23:13:57 +0000

    uiBuilder: actions: add points

--- uiBuilder/actions.lua
+++ uiBuilder/actions.lua
@@ -4,3 +4,11 @@ local builder = ns.builder
 builder.addAction("size", function(self, value)
 	self:SetSize(unpack(value))
 end)
+
+builder.addAction("points", function(self, value)
+
+	for i, point in ipairs(value) do
+		self:SetPoint(unpack(point))
+	end
+
+end)
