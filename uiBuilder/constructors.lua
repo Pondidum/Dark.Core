0c0e29a4d228c48c9499c888d463663585c74bf4
andy <andy@andydote.co.uk>
2014-02-15 23:13:36 +0000

    uiBuilder: add checkbox and frame constructors

--- uiBuilder/constructors.lua
+++ uiBuilder/constructors.lua
@@ -5,10 +5,10 @@ local buildName = function(parent, name)
 	return name:gsub("$parent", parent:GetName())
 end
 
-builder.addConstructor("checkbox", function(parent, config)
-	return CreateFrame("checkbox", buildName(parent, config.name), parent)
-end)
-
 builder.addConstructor("frame", function(parent, config)
 	return CreateFrame("frame", buildName(parent, config.name), parent)
+end)
+
+builder.addConstructor("checkbox", function(parent, config)
+	return CreateFrame("checkbox", buildName(parent, config.name), parent)
 end)
