154bcdb79d368528c170c3e70d25d4684122fdab
andy <andy@andydote.co.uk>
2014-02-16 18:37:50 +0000

    uibuilder: actions: add name parent replacing

--- uiBuilder/actions.lua
+++ uiBuilder/actions.lua
@@ -1,14 +1,20 @@
 local addon, ns = ...
 local builder = ns.builder
 
+local buildName = function(parent, name)
+	return name:gsub("$parent", parent:GetName())
+end
+
 builder.addAction("size", function(self, value)
 	self:SetSize(unpack(value))
 end)
 
 builder.addAction("points", function(self, value)
 
-	for i, point in ipairs(value) do
-		self:SetPoint(unpack(point))
+	for i, array in ipairs(value) do
+		local point, other, otherPoint, x, y = unpack(array)
+
+		self:SetPoint(point, buildName(self:GetParent(), other), otherPoint, x, y)
 	end
 
 end)
